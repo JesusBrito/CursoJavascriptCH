adaf1a5328c522f03a4d995dffa4a961673e0dd3
jesusbrito <udvuvvdu29>
2022-07-04 20:44:30 -0500

    Ejemplos clase

--- Ejemplos clase 9/index.js
+++ Ejemplos clase 9/index.js
@@ -1,41 +1,135 @@
-let botonUno = document.getElementById("btnEventoUno");
-let botonDos = document.getElementById("btnEventoDos");
+// let botonUno = document.getElementById("btnEventoUno")
+// let botonDos = document.getElementById("btnEventoDos")
 
-botonUno.addEventListener("click", () => {
-  alert("Se oprimió el botón uno");
-});
+// botonUno.addEventListener("click", () => {
+//   alert("Se oprimió el boton uno")
+// })
 
-botonDos.onclick = () => {
-  alert("Se oprimió el botón dos");
-};
+// botonDos.onclick = () => {
+//   alert("Se oprimió el botón dos")
+// }
 
-function mostrarAlert() {
-  alert("Se oprimió el botón tres");
+// function mostrarAlert(){
+//   alert("Se oprimió el botón tres")
+// }
+
+// let boton = document.getElementById("boton")
+
+// boton.onmousedown = () => {
+//   console.log("Evento mouse down")
+// }
+
+// boton.onmouseup = () => {
+//   console.log("Evento mouse up")
+// }
+
+// boton.onmouseover = () => {
+//   console.log("Evento mouse over")
+// }
+
+// boton.onmouseout = () => {
+//   console.log("Evento mouse out")
+// }
+
+// boton.onmousemove = () => {
+//   console.log("Evento mouse move")
+// }
+
+/*
+let inputNombre = document.getElementById("nombre");
+let inputEdad = document.getElementById("edad");
+*/
+/*
+inputNombre.onkeydown = () => {
+  alert("Se oprimió un boton, evento down")
 }
+*/
 
-let input1 = document.getElementById("nombre");
-let input2 = document.getElementById("edad");
-input1.onchange = (event) => {
-  console.log(event.target.value);
-};
-input2.onchange = (event) => {
-  console.log(event.target.value);
+/*
+inputNombre.onkeyup = () => {
+  alert("Se oprimió un boton, evento up")
+}*/
+
+/*
+inputNombre.onchange = () => {
+  console.log("se ejecuta evento onchange, input nombre");
 };
 
-let miFormulario = document.getElementById("formulario");
+inputEdad.onchange = () => {
+  console.log("se ejecuta evento onchange, input edad");
+};
+*/
+
+/*
+inputNombre.oninput = () => {
+  console.log("se ejecuta evento oninput, input nombre");
+};
+
+inputEdad.oninput = () => {
+  console.log("se ejecuta evento oninput, input edad");
+};
+*/
+
+let formulario = document.getElementById("formulario");
 let inputNombre = document.getElementById("inputNombreProducto");
 let inputPrecioCompra = document.getElementById("inputPrecioCompra");
 let inputPrecioVenta = document.getElementById("inputPrecioVenta");
 let inputCantidad = document.getElementById("inputCantidad");
 
-miFormulario.addEventListener("submit", validarFormulario);
+let productos = [];
+
+class Productos {
+  constructor(
+    nombre,
+    precioCompra,
+    precioVenta,
+    cantidad,
+    precioCompraTotal,
+    precioVentaTotal
+  ) {
+    this.nombre = nombre.toUpperCase();
+    this.precioCompra = precioCompra;
+    this.precioVenta = precioVenta;
+    this.cantidad = cantidad;
+    this.precioCompraTotal = precioCompraTotal;
+    this.precioVentaTotal = precioVentaTotal;
+  }
+}
+
+formulario.onsubmit = (event) => validarFormulario(event);
 
 function validarFormulario(event) {
   event.preventDefault();
-  console.log(event.target);
+  let nombre = inputNombre.value;
+  let precioCompra = parseFloat(inputPrecioCompra.value);
+  let precioVenta = parseFloat(inputPrecioVenta.value);
+  let cantidad = parseInt(inputCantidad.value);
 
-  console.log(inputNombre.value);
-  console.log(inputPrecioCompra.value);
-  console.log(inputPrecioVenta.value);
-  console.log(inputCantidad.value);
+  let totalPrecioCompra = cantidad * precioCompra;
+  let totalPrecioVenta = cantidad * precioVenta;
+
+  let producto = new Productos(
+    nombre,
+    precioCompra,
+    precioVenta,
+    cantidad,
+    totalPrecioCompra,
+    totalPrecioVenta
+  );
+  productos.push(producto);
+
+  if(nombre.toLowerCase() === "laptop"){
+    alert("es una laptop")
+  }
+
+  /*
+    inputNombre.value = "";
+    inputPrecioCompra.value = "";
+    inputPrecioVenta.value = "";
+    inputCantidad.value = "";
+  */
+
+  formulario.reset();
+
+  console.log(productos);
 }
